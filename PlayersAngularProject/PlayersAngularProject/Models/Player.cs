09f57b8e07fa7de96287733224b0933e377056e8
Letjobana <suzanm@gerbertech.co.za>
2022-03-11 13:26:43 +0200

    Added a new class Player

---
+++ PlayersAngularProject/PlayersAngularProject/Models/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlayersAngularProject.Models
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int ShirtNo { get; set; }
+        public int Appearances { get; set; }
+        public int Goals { get; set; }
+        public PlayerPositions Position { get; set; }
+
+        [NotMapped]
+        public string PositionName => Position.ToString();
+
+        [NotMapped]
+        public decimal GoalsPerMatch => Math.Round((decimal)Goals / (decimal)Appearances, 2);
+    }
+
+    public enum PlayerPositions
+    {
+        Goalkeeper,
+        Defender,
+        Midfielder,
+        Forward
+    }
+}
