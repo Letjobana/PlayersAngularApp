68941375406eb30cea79159453310d4a01fd0c68
Letjobana <suzanm@gerbertech.co.za>
2022-03-11 13:36:47 +0200

    Added Database connection

---
+++ PlayersAngularProject/PlayersAngularProject/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlayersAngularProject.Models;
+
+namespace PlayersAngularProject.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Player> Players { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>().HasData(
+                new Player() { Id = 1, Name = "Juan Mata", ShirtNo = 8, Appearances = 268, Goals = 54, Position = PlayerPositions.Forward },
+                new Player() { Id = 2, Name = "Paul Pogba", ShirtNo = 6, Appearances = 130, Goals = 28, Position = PlayerPositions.Midfielder },
+                new Player() { Id = 3, Name = "David de Gea", ShirtNo = 1, Appearances = 335, Goals = 0, Position = PlayerPositions.Goalkeeper },
+                new Player() { Id = 4, Name = "Phil Jones", ShirtNo = 4, Appearances = 200, Goals = 2, Position = PlayerPositions.Defender }
+            );
+        }
+    }
+}
--- PlayersAngularProject/PlayersAngularProject/Startup.cs
+++ PlayersAngularProject/PlayersAngularProject/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PlayersAngularProject.Persistance;
 
 namespace PlayersAngularProject
 {
@@ -21,6 +23,7 @@ namespace PlayersAngularProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
