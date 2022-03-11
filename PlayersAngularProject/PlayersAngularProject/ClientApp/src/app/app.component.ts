b34528f7b6f1c4522364e2c5d745973c5bc61e79
Letjobana <suzanm@gerbertech.co.za>
2022-03-11 19:44:53 +0200

    Added player component

--- PlayersAngularProject/PlayersAngularProject/ClientApp/src/app/app.component.ts
+++ PlayersAngularProject/PlayersAngularProject/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public players: Player[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Player[]>(baseUrl + 'api/Player').subscribe(result => {
+      this.players = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Player {
+  id: number;
+  name: string;
+  shirtNo: number;
+  appearances: number;
+  goals: number;
+  positionName: number;
+  goalsPerMatch: number;
 }
---
+++ PlayersAngularProject/PlayersAngularProject/Controllers/PlayersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayersAngularProject.Models;
+using PlayersAngularProject.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PlayersAngularProject.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public PlayersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Player>>> GetPlayers()
+        {
+            try
+            {
+                return await context.Players.ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
